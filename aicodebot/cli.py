cbdec70ee9d0eecebd6983650f1757d73efe172c
Nick Sullivan <nick@sullivanflock.com>
2023-06-21 22:24:17 -0700

    Update CLI to reflect correct number of files committed 📝

--- aicodebot/cli.py
+++ aicodebot/cli.py
@@ -113,7 +113,7 @@ def commit(verbose, max_tokens, yes):
     if yes or click.confirm("Do you want to commit the changes?"):
         # Commit the changes using the temporary file for the commit message
         exec_and_get_output(["git", "commit", "-a", "-F", temp_file_name])
-        console.print(f"✅ {len(files)} files committed.")
+        console.print(f"✅ {len(files.splitlines())} files committed.")
 
     # Delete the temporary file
     Path.unlink(temp_file_name)
