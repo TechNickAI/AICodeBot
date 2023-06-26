82c89ea5ef475e57c1d4cd0448d1e2f02caafbc3
Nick Sullivan <nick@sullivanflock.com>
2023-06-26 17:09:44 -0600

    Add test_review function to test workflow

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -18,3 +18,24 @@ def test_commit_command(cli_runner, temp_git_repo):
         repo = Repo(temp_git_repo.working_dir)
         last_commit_message = repo.head.commit.message
         assert len(last_commit_message) > 10
+
+
+@pytest.mark.skipif(os.getenv("OPENAI_API_KEY") is None, reason="Skipping live tests without an API key.")
+def test_review(cli_runner, temp_git_repo):
+    with cli_runner.isolated_filesystem():
+        os.chdir(temp_git_repo.working_dir)  # change to the temporary repo directory
+
+        # Add a new file
+        with Path.open("test.txt", "w") as f:
+            f.write("Adding a new line.")
+
+        repo = Repo(temp_git_repo.working_dir)
+        # Stage the new file
+        repo.git.add("test.txt")
+
+        # Run the review command
+        result = cli_runner.invoke(cli, ["review"])
+
+        # Check that the review command ran successfully
+        assert result.exit_code == 0
+        assert len(result.output) > 20
