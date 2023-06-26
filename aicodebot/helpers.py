4b5db0603a7f93dd74373d8071b47bef8edf769c
Nick Sullivan <nick@sullivanflock.com>
2023-06-26 16:44:09 -0600

    Add tiktoken library for tokenization and update git_diff_context function
    The tiktoken library is added to the project to provide tokenization functionality.
    The get_token_length function is added to helpers.py to get the number of tokens in a string using the tiktoken library.
    The git_diff_context function is updated to include new files in the diff output and to provide 10 lines of context.
    Tests are added for the new functions.

--- aicodebot/helpers.py
+++ aicodebot/helpers.py
@@ -1,4 +1,41 @@
-import subprocess
+from pathlib import Path
+import subprocess, tiktoken
+
+
+def get_token_length(text, model="gpt-3.5-turbo"):
+    """Get the number of tokens in a string using the tiktoken library."""
+    encoding = tiktoken.encoding_for_model(model)
+    tokens = encoding.encode(text)
+    return len(tokens)
+
+
+def git_diff_context(commit=None):
+    """Get a text representation of the git diff for the current commit or staged files, including new files"""
+    base_git_diff = ["git", "diff", "-U10"]  # Tell diff to provide 10 lines of context
+
+    if commit:
+        return exec_and_get_output(["git", "show", commit])
+    else:
+        staged_files = exec_and_get_output(["git", "diff", "--cached", "--name-only"]).splitlines()
+        if staged_files:
+            file_status = exec_and_get_output(["git", "diff", "--cached", "--name-status"]).splitlines()
+        else:
+            file_status = exec_and_get_output(["git", "diff", "--name-status"]).splitlines()
+
+        diffs = []
+        for status in file_status:
+            status_code, file_name = status.split("\t")
+            if status_code == "A":
+                # If the file is new, include the entire file content
+                contents = Path.read_text(file_name)
+                diffs.append(f"## New file added: {file_name}")
+                diffs.append(contents)
+            else:
+                # If the file is not new, get the diff
+                diffs.append(f"## File changed: {file_name}")
+                diffs.append(exec_and_get_output(base_git_diff + ["--cached", "--", file_name]))
+
+        return "\n".join(diffs)
 
 
 def exec_and_get_output(command):
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,55 @@
-from aicodebot.helpers import exec_and_get_output
-import pytest
+from aicodebot.helpers import exec_and_get_output, get_token_length, git_diff_context
+from git import Repo
+from pathlib import Path
+import os, pytest
+
+
+def test_get_token_length():
+    text = ""
+    assert get_token_length(text) == 0
+
+    text = "Code with heart, align AI with humanity. ❤️🤖"
+    assert get_token_length(text) == 14
+
+
+def test_git_diff_context(cli_runner, temp_git_repo):
+    with cli_runner.isolated_filesystem():
+        os.chdir(temp_git_repo.working_dir)  # change to the temporary repo directory
+
+        # Add a new file
+        with Path.open("test.txt", "w") as f:
+            f.write("Adding a new line.")
+
+        # Get the diff for changes
+        diff = git_diff_context()
+        assert "Adding a new line." in diff
+
+        # Commit the changes
+        repo = Repo(temp_git_repo.working_dir)
+        repo.git.add("test.txt")
+        repo.git.commit("-m", "Add test.txt")
+
+        # Get the diff for the commit
+        commit = repo.head.commit.hexsha
+        diff = git_diff_context(commit)
+        assert "Adding a new line." in diff
+
+        # Modify the file
+        with Path.open("test.txt", "a") as f:
+            f.write("Adding another line.")
+
+        # Get the diff for changes
+        diff = git_diff_context()
+        assert "Adding another line." in diff
+
+        # Commit the changes
+        repo.git.add("test.txt")
+        repo.git.commit("-m", "Modify test.txt")
+
+        # Get the diff for the commit
+        commit = repo.head.commit.hexsha
+        diff = git_diff_context(commit)
+        assert "Adding another line." in diff
 
 
 def test_exec_and_get_output_success():
