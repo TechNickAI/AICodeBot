f303cd743ebb445f5ab8273d3328d61932feb73a
Nick Sullivan <nick@sullivanflock.com>
2023-07-03 16:44:04 -0700

    Refactor test skipping condition for missing API key
    This commit refactors the condition used to skip tests when the OPENAI_API_KEY environment variable is not set. The previous condition checked if the value of the variable was `None`, but this has been changed to a more Pythonic way of checking if the variable is not set or empty. This change improves readability and ensures that tests are skipped even when the variable is set to an empty string.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,13 +9,13 @@ def test_version(cli_runner):
     assert aicodebot_version in result.output
 
 
-@pytest.mark.skipif(os.getenv("OPENAI_API_KEY") is None, reason="Skipping live tests without an API key.")
+@pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
 def test_fun_fact(cli_runner):
     result = cli_runner.invoke(cli, ["fun-fact"])
     assert result.exit_code == 0
 
 
-@pytest.mark.skipif(os.getenv("OPENAI_API_KEY") is None, reason="Skipping live tests without an API key.")
+@pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
 def test_alignment(cli_runner):
     result = cli_runner.invoke(cli, ["alignment"])
     assert result.exit_code == 0
@@ -28,7 +28,7 @@ def test_debug_success(cli_runner):
     assert "The command completed successfully." in result.output
 
 
-@pytest.mark.skipif(os.getenv("OPENAI_API_KEY") is None, reason="Skipping live tests without an API key.")
+@pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
 def test_debug_failure(cli_runner):
     result = cli_runner.invoke(cli, ["debug", "ls", "-9"])
     assert result.exit_code > 0
--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os, pytest
 
 
-@pytest.mark.skipif(os.getenv("OPENAI_API_KEY") is None, reason="Skipping live tests without an API key.")
+@pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
 def test_commit_command(cli_runner, temp_git_repo):
     with cli_runner.isolated_filesystem():
         os.chdir(temp_git_repo.working_dir)  # change to the temporary repo directory
@@ -20,7 +20,7 @@ def test_commit_command(cli_runner, temp_git_repo):
         assert len(last_commit_message) > 10
 
 
-@pytest.mark.skipif(os.getenv("OPENAI_API_KEY") is None, reason="Skipping live tests without an API key.")
+@pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
 def test_review(cli_runner, temp_git_repo):
     with cli_runner.isolated_filesystem():
         os.chdir(temp_git_repo.working_dir)  # change to the temporary repo directory
