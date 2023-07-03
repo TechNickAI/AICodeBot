398bc51bbac4536feb74163b98082d4b62d2766d
Nick Sullivan <nick@sullivanflock.com>
2023-07-03 16:53:31 -0700

    Include the output when the test fails

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,25 +5,25 @@ import os, pytest
 
 def test_version(cli_runner):
     result = cli_runner.invoke(cli, ["-V"])
-    assert result.exit_code == 0
+    assert result.exit_code == 0, f"Output: {result.output}"
     assert aicodebot_version in result.output
 
 
 @pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
 def test_fun_fact(cli_runner):
     result = cli_runner.invoke(cli, ["fun-fact"])
-    assert result.exit_code == 0
+    assert result.exit_code == 0, f"Output: {result.output}"
 
 
 @pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
 def test_alignment(cli_runner):
     result = cli_runner.invoke(cli, ["alignment"])
-    assert result.exit_code == 0
+    assert result.exit_code == 0, f"Output: {result.output}"
 
 
 def test_debug_success(cli_runner):
     result = cli_runner.invoke(cli, ["debug", "echo", "Hello, world!"])
-    assert result.exit_code == 0
+    assert result.exit_code == 0, f"Output: {result.output}"
     assert "echo Hello, world!" in result.output
     assert "The command completed successfully." in result.output
 
@@ -31,5 +31,5 @@ def test_debug_success(cli_runner):
 @pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
 def test_debug_failure(cli_runner):
     result = cli_runner.invoke(cli, ["debug", "ls", "-9"])
-    assert result.exit_code > 0
+    assert result.exit_code > 0, f"Output: {result.output}"
     assert "ls -9" in result.output
