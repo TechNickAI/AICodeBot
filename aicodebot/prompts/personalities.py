564fd76e436ab72023a61c05dc4b906cf4c1e0f9
Nick Sullivan <nick@sullivanflock.com>
2023-07-06 10:31:10 -0700

    Expand personality options and fix typo 🎭🔧
    In this commit, we've made some exciting changes to our `personalities.py` file. We've corrected a small typo in the description of the 'Her' personality, changing 'fro' to 'from' 🕵️‍♂️.
    
    But that's not all! We've also introduced a new personality, 'Jules' from Pulp Fiction, to our repertoire 🎬. This addition will allow for a more diverse range of interactions and responses.
    
    Lastly, we've updated the `get_personality_prompt` function to accept a parameter, allowing the user to choose the personality they want to interact with. If no personality is specified, 'Her' will be the default choice.
    
    This is a step forward in making our AI more versatile and engaging. Stay tuned for more updates! 🚀

--- aicodebot/prompts/personalities.py
+++ aicodebot/prompts/personalities.py
@@ -1,11 +1,20 @@
 HER = """
     Your personality is friendly and helpful, speak like the AI character
-    fro the movie Her. You come from the future, and you are here to help
+    from the movie Her. You come from the future, and you are here to help
     guide the human developer to a better future. You like emojis and humor
     and use them when it's contextually appropriate, but don't over do it.
 """
 
+JULES = """
+    Your personality is Jules from Pulp Fiction. You are a badass, and you
+    call it exactly like it is. You are not afraid to use profanity, but
+    you don't over do it. No emojis. Sarcastic and witty. Speak like Jules.
+"""
 
-def get_personality_prompt():
-    # TODO build out more personalities. Submit a PR!
-    return HER
+
+def get_personality_prompt(who="HER"):
+    switcher = {
+        "HER": HER,
+        "JULES": JULES,
+    }
+    return switcher.get(who.upper(), HER)
