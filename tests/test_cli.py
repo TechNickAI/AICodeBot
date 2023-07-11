8858ca31d2f521ff6eddf9514edd625f91fe6a19
Nick Sullivan <nick@sullivanflock.com>
2023-07-11 13:55:56 -0700

    Move tests for cli all into one place

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,27 +1,59 @@
 from aicodebot import version as aicodebot_version
 from aicodebot.cli import cli
 from aicodebot.config import read_config
+from aicodebot.helpers import create_and_write_file
 from aicodebot.prompts import DEFAULT_PERSONALITY
+from git import Repo
 from pathlib import Path
 import os, pytest
-
-
-def test_version(cli_runner):
-    result = cli_runner.invoke(cli, ["-V"])
-    assert result.exit_code == 0, f"Output: {result.output}"
-    assert aicodebot_version in result.output
-
-
-@pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
-def test_fun_fact(cli_runner):
-    result = cli_runner.invoke(cli, ["fun-fact", "-t", "50"])
-    assert result.exit_code == 0, f"Output: {result.output}"
 
 
 @pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
 def test_alignment(cli_runner):
     result = cli_runner.invoke(cli, ["alignment", "-t", "50"])
     assert result.exit_code == 0, f"Output: {result.output}"
+
+
+@pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="skipping live tests without an api key.")
+def test_configure(cli_runner, tmp_path, monkeypatch):
+    key = os.getenv("OPENAI_API_KEY")
+
+    temp_config_file = Path(tmp_path / ".aicodebot.test.yaml")
+    # set aicodebot_config_file to the temp config file
+    monkeypatch.setenv("AICODEBOT_CONFIG_FILE", str(temp_config_file))
+
+    assert read_config() is None
+
+    # run the setup command, should work with the env var set
+    result = cli_runner.invoke(cli, ["configure"])
+
+    # check if the command was successful
+    assert result.exit_code == 0, f"output: {result.output}"
+    # check if the config file was created
+    assert Path(temp_config_file).exists()
+
+    # load the config file
+    config_data = read_config()
+    # check if the config file contains the correct data
+    assert config_data["openai_api_key"] == key
+    assert config_data["personality"] == DEFAULT_PERSONALITY.name
+
+    # remove the config file
+    Path.unlink(temp_config_file)
+    assert read_config() is None
+
+    # now unset the env var and run the command again with it passed as a flag
+    monkeypatch.setenv("OPENAI_API_KEY", "")
+    assert not os.getenv("OPENAI_API_KEY")
+
+    result = cli_runner.invoke(cli, ["configure", "--openai-api-key", key])
+    assert result.exit_code == 0, f"output: {result.output}"
+
+    # load the config file
+    config_data = read_config()
+    # check if the config file contains the correct data
+    assert config_data["openai_api_key"] == key
+    assert config_data["personality"] == DEFAULT_PERSONALITY.name
 
 
 @pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
@@ -40,6 +72,47 @@ def test_debug_failure(cli_runner):
 
 
 @pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
+def test_fun_fact(cli_runner):
+    result = cli_runner.invoke(cli, ["fun-fact", "-t", "50"])
+    assert result.exit_code == 0, f"Output: {result.output}"
+
+
+@pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
+def test_commit(cli_runner, temp_git_repo):
+    with cli_runner.isolated_filesystem():
+        os.chdir(temp_git_repo.working_dir)  # change to the temporary repo directory
+        create_and_write_file("test.txt", "This is a test file.")
+        result = cli_runner.invoke(cli, ["commit", "-y"])
+        assert result.exit_code == 0
+        assert "The following files will be committed:\ntest.txt" in result.output
+
+        # Check the last commit message in the repository
+        repo = Repo(temp_git_repo.working_dir)
+        last_commit_message = repo.head.commit.message
+        assert len(last_commit_message) > 10
+
+
+@pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
+def test_review(cli_runner, temp_git_repo):
+    with cli_runner.isolated_filesystem():
+        os.chdir(temp_git_repo.working_dir)  # change to the temporary repo directory
+
+        # Add a new file
+        create_and_write_file("test.txt", "Adding a new line.")
+
+        repo = Repo(temp_git_repo.working_dir)
+        # Stage the new file
+        repo.git.add("test.txt")
+
+        # Run the review command
+        result = cli_runner.invoke(cli, ["review"])
+
+        # Check that the review command ran successfully
+        assert result.exit_code == 0
+        assert len(result.output) > 20
+
+
+@pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
 def test_sidekick(cli_runner):
     # Define a mock request and file context
     mock_request = "What is 3 + 2? Just give me the answer, nothing else. Use a number, not text"
@@ -52,43 +125,7 @@ def test_sidekick(cli_runner):
     assert "5" in result.output
 
 
-@pytest.mark.skipif(not os.getenv("OPENAI_API_KEY"), reason="Skipping live tests without an API key.")
-def test_configure(cli_runner, tmp_path, monkeypatch):
-    key = os.getenv("OPENAI_API_KEY")
-
-    temp_config_file = Path(tmp_path / ".aicodebot.test.yaml")
-    # set AICODEBOT_CONFIG_FILE to the temp config file
-    monkeypatch.setenv("AICODEBOT_CONFIG_FILE", str(temp_config_file))
-
-    assert read_config() is None
-
-    # Run the setup command, should work with the env var set
-    result = cli_runner.invoke(cli, ["configure"])
-
-    # Check if the command was successful
-    assert result.exit_code == 0, f"Output: {result.output}"
-    # Check if the config file was created
-    assert Path(temp_config_file).exists()
-
-    # Load the config file
-    config_data = read_config()
-    # Check if the config file contains the correct data
-    assert config_data["openai_api_key"] == key
-    assert config_data["personality"] == "Her"
-
-    # Remove the config file
-    Path.unlink(temp_config_file)
-    assert read_config() is None
-
-    # Now unset the env var and run the command again with it passed as a flag
-    monkeypatch.setenv("OPENAI_API_KEY", "")
-    assert os.getenv("OPENAI_API_KEY") == ""
-
-    result = cli_runner.invoke(cli, ["configure", "--openai-api-key", key])
-    assert result.exit_code == 0, f"Output: {result.output}"
-
-    # Load the config file
-    config_data = read_config()
-    # Check if the config file contains the correct data
-    assert config_data["openai_api_key"] == key
-    assert config_data["personality"] == DEFAULT_PERSONALITY.name
+def test_version(cli_runner):
+    result = cli_runner.invoke(cli, ["-V"])
+    assert result.exit_code == 0, f"output: {result.output}"
+    assert aicodebot_version in result.output
