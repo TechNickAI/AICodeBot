db8cde8a45462b3d9bc7f6d93f92143cfe8e94c2
Nick Sullivan <nick@sullivanflock.com>
2023-07-20 15:15:44 -0700

    Remove the check for main in setup.py, we use aicodebot/__init__.py to store the version now

--- setup.py
+++ setup.py
@@ -10,30 +10,29 @@ requirements = Path("requirements/requirements.in").read_text(encoding="utf-8").
 # Remove lines that are empty or start with # (comments)
 requirements = [line for line in requirements if line and not line.startswith("#")]
 
-if __name__ == "__main__":  # Only run setup if this is the main file (allows this file to be imported for __version__)
-    setup(
-        name="aicodebot",
-        python_requires=">=3.9",
-        version=version,
-        url="https://github.com/gorillamania/AICodeBot",
-        author="Nick Sullivan",
-        description="AI-powered tool for developers, simplifying coding tasks and improving workflow efficiency.",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        keywords="AI, coding, assistant, pair-programming, automation, productivity, workflow, artificial intelligence",
-        install_requires=requirements,
-        entry_points={
-            "console_scripts": [
-                "aicodebot = aicodebot.cli:cli",
-            ],
-        },
-        classifiers=[
-            "Development Status :: 4 - Beta",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: GNU Affero General Public License v3",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Programming Language :: Python :: 3.11",
+setup(
+    name="aicodebot",
+    python_requires=">=3.9",
+    version=version,
+    url="https://github.com/gorillamania/AICodeBot",
+    author="Nick Sullivan",
+    description="AI-powered tool for developers, simplifying coding tasks and improving workflow efficiency.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="AI, coding, assistant, pair-programming, automation, productivity, workflow, artificial intelligence",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "aicodebot = aicodebot.cli:cli",
         ],
-    )
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
