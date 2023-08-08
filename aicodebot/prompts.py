14a603f0e9d536b7e83cba308540319ef57f8f67
Nick Sullivan <nick@sullivanflock.com>
2023-08-08 18:27:24 +0200

    Turn off patching until we can get it reliably working

--- aicodebot/prompts.py
+++ aicodebot/prompts.py
@@ -237,7 +237,8 @@ You respond in GitHub markdown format, which is then parsed by the Python rich M
 library to produce a rich terminal output.
 
 """
-    + PATCH_FORMAT_EXPLANATION
+    # Turn off patch format until we can get it reliably working with LLMs
+    # + PATCH_FORMAT_EXPLANATION
     + """
 
 {context}
